72353dd9ed33ebec479a0fc2d76705c2d0db7c5e
danderson00 <danderson00@gmail.com>
2014-12-03 18:55:45 +1100

    Moved from tribe repository

---
+++ Samples/Transitions/controls.js
@@ -0,0 +1,10 @@
+﻿T.registerModel(function(pane) {
+    var self = this;
+    var target = 1;
+    this.transition = ko.observable('fade');    
+
+    this.execute = function () {
+        target = target % 3 + 1;
+        T.transition(T.nodeFor('.target'), self.transition()).to('target' + target);
+    };
+});
