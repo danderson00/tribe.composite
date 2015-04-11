c62e180282aa72ebbd2dc8de1a98273719fa86b9
Dale Anderson <danderson00@gmail.com>
2015-04-10 20:06:05 -0700

    Pipeline handles any A+ promise

--- Source/Types/Pipeline.js
+++ Source/Types/Pipeline.js
@@ -23,9 +23,21 @@
             // could possibly improve debugging in non-production scenarios by omitting the fail handler
             // using .done without a fail handler should mean the exception is unhandled, allowing it
             // to be caught by the debugger easily.
-            $.when(thisEvent(target, context))
-                .done(executeNextEvent)
-                .fail(handleFailure);
+            // $.when(thisEvent(target, context))
+            //     .done(executeNextEvent)
+            //     .fail(handleFailure);
+
+            // this handles promises returned from any other A+ compliant promise library
+            var eventResult = thisEvent(target, context);
+            if(eventResult && typeof eventResult.then === 'function') {
+                eventResult.then(executeNextEvent, handleFailure);
+            } else {
+                try {
+                    executeNextEvent();
+                } catch(error) {
+                    handleFailure(error);
+                }
+            }
 
             function handleFailure(error) {
                 promise.reject(error);
